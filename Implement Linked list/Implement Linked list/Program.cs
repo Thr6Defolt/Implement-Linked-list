fa579afe6f63758f3c632950e883f484b3d665b5
Thr6Defolt <43175754+Thr6Defolt@users.noreply.github.com>
2023-11-05 20:14:27 +0200

    Add files via upload

---
+++ Implement Linked list/Implement Linked list/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Implement_Linked_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SinglyLinkedList list = new SinglyLinkedList();
+            list.Add(5);
+            list.Add(8);
+            list.Add(9);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Delete(2);
+            list.Delete(5);
+            list.Delete(1);
+            list.Delete(3);
+            list.Delete(4);
+
+
+        }
+    }
+}
---
+++ Implement Linked list/Implement Linked list/SinglyLinkedList.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Implement_Linked_list
+{
+    public class SinglyLinkedList
+    {
+        public Node head;
+        public void Add(int newData)
+        {
+            Node newNode = new Node(newData);
+            Node current = head;
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;
+            }
+            Console.WriteLine(newData + " added.");
+        }
+        public void Delete(int newData)
+        {
+            if (head != null)
+            {
+                if (head.data == newData)
+                {
+                    head = head.next;
+                    Console.WriteLine(newData + " deleted.");
+                }
+                else
+                {
+                    Node current = head;
+                    while (current.next != null && current.next.data != newData)
+                    {
+                        current = current.next;
+                    }
+                    if (current.next != null && current.next.data == newData)
+                    {
+                        current.next = current.next.next;
+                        Console.WriteLine(newData + " deleted.");
+                    }
+                    else
+                    {
+                        Console.WriteLine(newData + " not found.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(newData + " not found.");
+            }
+        }
+    }
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
